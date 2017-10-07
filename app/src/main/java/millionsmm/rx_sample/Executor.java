1421888774be47f173d55b2f6ec591c3a4b279e4
millionsmm <linweipeng96@qq.com>
2017-10-07 16:48:02 +0800

    - try to create my own threadPool

---
+++ app/src/main/java/millionsmm/rx_sample/Executor.java
@@ -0,0 +1,18 @@
+package millionsmm.rx_sample;
+
+/**
+ * Created by Wilber
+ * on 7/10/2017.
+ */
+
+public interface Executor<Work extends Runnable> {
+    void execute(Work work);
+
+    void shutdown();
+
+    void addWorks(Runnable runnable);
+
+    void removeWorks(int num);
+
+    void getWorkSize();
+}
---
+++ app/src/main/java/millionsmm/rx_sample/MyThreadPool.java
@@ -0,0 +1,35 @@
+package millionsmm.rx_sample;
+
+
+/**
+ * Created by Wilber
+ * on 7/10/2017.
+ */
+
+public class MyThreadPool<Work extends Runnable> implements Executor<Work> {
+
+    @Override
+    public void execute(Work work) {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public void addWorks(Runnable runnable) {
+
+    }
+
+    @Override
+    public void removeWorks(int num) {
+
+    }
+
+    @Override
+    public void getWorkSize() {
+
+    }
+}
